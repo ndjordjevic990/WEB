45624fcf6b2310e58254ad089d6c21c14e9b8631
ndjordjevic990 <n.djordjevic990@gmail.com>
2021-06-11 23:48:27 +0200

    11.06.

--- DOM/events/superMario/index.js
+++ DOM/events/superMario/index.js
@@ -1,22 +1,67 @@
 var mario = document.querySelector("#mario");
-var marioRuns = document.querySelector("#running");
-var grass = document.querySelector("container");
-var xPos = 0;
-var yPos = "0px";
+var marioRunning = document.querySelector("#running");
+var marioRunningLeft = document.querySelector("#running-left");
+var grass = document.querySelector(".container");
+var timer;
+var run = false;
 
 document.addEventListener("keydown", startRunning);
+document.addEventListener("keydown", startRunningLeft);
 document.addEventListener("keyup", stopRunning);
 
+function runningMario() {
+  mario.style.display = "none";
+  marioRunning.style.display = "block";
+}
+function runningMarioLeft() {
+  mario.style.display = "none";
+  marioRunningLeft.style.display = "block";
+}
+
+function holdYourGround() {
+  mario.style.display = "block";
+  marioRunning.style.display = "none";
+  marioRunningLeft.style.display = "none";
+}
+
 function startRunning(e) {
-  if (e.keyCode == 39) {
-    mario.style.display = "none";
-    marioRuns.style.display = "block";
+  if (e.keyCode == 39 && !run) {
+    runningMario();
+    timer = setInterval(movingGrass, 60);
+    run = true;
+  } else {
+    return;
+  }
+}
+
+function startRunningLeft(e) {
+  if (e.keyCode == 37 && !run) {
+    runningMarioLeft();
+    timer = setInterval(movingGrassLeft, 60);
+    run = true;
+  } else {
+    return;
   }
 }
 
 function stopRunning(e) {
-  if (e.keyCode == 39) {
-    mario.style.display = "block";
-    marioRuns.style.display = "none";
+  if (e.keyCode == 39 || e.keyCode == 37) {
+    holdYourGround();
+    clearInterval(timer);
+    run = false;
   }
+}
+
+// *******************************
+// Moving grass along with Mario
+// ******************************
+grass.style.backgroundPositionX = "100px";
+
+function movingGrass() {
+  grass.style.backgroundPositionX =
+    parseInt(grass.style.backgroundPositionX) - 10 + "px";
+}
+function movingGrassLeft() {
+  grass.style.backgroundPositionX =
+    parseInt(grass.style.backgroundPositionX) + 10 + "px";
 }
